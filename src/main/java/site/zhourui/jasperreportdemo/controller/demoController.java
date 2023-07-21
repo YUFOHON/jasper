dcd03e24bd41d7e79bf29ca66e49acc931375c54
zr <2437264464@qq.com>
2023-07-21 17:01:53 +0800

    springBoot 整合JasperReport基础案例(固定报表)-中文显示问题

--- src/main/java/site/zhourui/jasperreportdemo/controller/demoController.java
+++ src/main/java/site/zhourui/jasperreportdemo/controller/demoController.java
@@ -1,9 +1,6 @@
 package site.zhourui.jasperreportdemo.controller;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.*;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +11,8 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 /**
@@ -44,5 +43,26 @@ public class demoController {
             os.flush();
             os.close();
         }
+    }
+
+    @RequestMapping("/test2")
+    public void test2(HttpServletResponse response) throws Exception{
+        Resource resource = new ClassPathResource("templates/demo2.jasper");
+        FileInputStream is = new FileInputStream(resource.getFile());
+        ServletOutputStream os = response.getOutputStream();
+        try {
+            /**
+             * 创建JasperPrint对象
+             * 数据填充
+             * is:inputstream params：参数填充 DataSource：数据源填充
+             */
+            JasperPrint jasperPrint = JasperFillManager.fillReport(is, new HashMap<>(), new JREmptyDataSource());
+            // 写入pdf数据
+            JasperExportManager.exportReportToPdfStream(jasperPrint, os);
+        } finally {
+            os.flush();
+            os.close();
+        }
+
     }
 }
